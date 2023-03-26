68f7e96602a6b41d23b1de81e542936a4c7b7b3a
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-03-26 13:29:53 +0000

    変数の練習問題

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 7;
+        String str = "大山　哲明";
+        System.out.println(str);
+        str = "ノーペインノーゲイン";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
